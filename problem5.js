8441517a14aa685431b1b331968e6cf01683f44f
mohsin6862 <mohsinctg2206@gmail.com>
2023-02-06 20:02:56 +0600

    problem5 solution added

---
+++ problem5.js
@@ -0,0 +1,17 @@
+// এমন একটা ফাংশন বানাবে যেটিতে তুমি ইনপুট হিসেবে পানির volume ইন্সারট করবে মিলিলিটারে। এই মিলিলিটারকে লিটারে কনভার্ট করে  ফাংশন তোমাকে রিটার্ন করবে।
+
+
+
+
+
+function miliToLitter( ml){
+
+    // 1 Milliliter = 0.001 Liters
+    let litter = ml * 0.001;
+    console.log(litter);
+
+
+
+}
+
+miliToLitter(5000);
